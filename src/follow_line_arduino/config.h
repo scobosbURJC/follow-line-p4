fc81ca6437fc3ec42f959ab6d344c79540206032
scobosb <s.cobosb.2022@alumnos.urjc.es>
2024-12-05 13:44:36 +0100

    Provisional config.h values

---
+++ src/follow_line_arduino/config.h
@@ -0,0 +1,43 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ----------- PINS DEFINITION --------------
+// Ultrasonic sensor
+#define TRIG_PIN 13  
+#define ECHO_PIN 12  
+
+// IR sensors
+#define PIN_ITR20001_LEFT   A2
+#define PIN_ITR20001_MIDDLE A1
+#define PIN_ITR20001_RIGHT  A0
+
+// Enable/Disable motor control.
+//  HIGH: motor control enabled
+//  LOW: motor control disabled
+#define PIN_Motor_STBY 3
+
+// Group A Motors (Right Side)
+// PIN_Motor_AIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_AIN_1 7
+// PIN_Motor_PWMA: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMA 5
+
+// Group B Motors (Left Side)
+// PIN_Motor_BIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_BIN_1 8
+// PIN_Motor_PWMB: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMB 6
+// ------------------------------------------
+
+// ----------- RGB COLORS ----------------
+#define RED {255,0,0}
+#define GREEN {0,255,0}
+// ---------------------------------------
+
+// ----------- ULTRASONIC UTILS --------------
+#define OBSTACLE_DIST_LIMIT 8 // Unit: cm
+#define SOUND_SPEED 0.0343 // Unit: cm/us
+#define TRIG_DELAY 10 // Unit: us
+// -------------------------------------------
+
+#endif CONFIG_H
