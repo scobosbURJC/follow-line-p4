b28f610a8f5952e0e7199261aa8b2fb51bb8fdf7
scobosb <s.cobosb.2022@alumnos.urjc.es>
2024-12-10 18:20:20 +0100

    obstacle and follow line tasks

--- src/follow_line_arduino/config.h
+++ src/follow_line_arduino/config.h
@@ -33,9 +33,12 @@
 // ------------------------------------------
 
 // --------- NEOPIXEL UTILS --------------
-#define RED {255,0,0}
+#define RED   {255,0,0}
 #define GREEN {0,255,0}
+#define RGB_COMPONENTS 3
+
 #define NUM_LEDS 1
+#define LED_BRIGHTNESS 20
 // ---------------------------------------
 
 // ----------- ULTRASONIC UTILS --------------
@@ -44,8 +47,51 @@
 #define TRIG_DELAY 10 // Unit: us
 // -------------------------------------------
 
+// ----------- MOTOR UTILS --------------
+#define MOTOR_STOP 0
+#define MOTOR_MAX_SPEED 255
+// --------------------------------------
+
+// ---------- IR UTILS ------------
+#define IR_BLACK_THRESHOLD 800                    //  !!!!! NOTA PARA SERGIO !!!!! He cambiado esto por black threshold por lo de que se envía un valor alto cuando es negro, 
+                                                  //                               entonces decimos que será negro si el valor devuelto es mayor que IR_BLACK_THRESHOLD
+
+#define IR_SENSORS_LIST {PIN_ITR20001_LEFT, PIN_ITR20001_MIDDLE, PIN_ITR20001_RIGHT}
+#define N_IR_SENSORS 3
+// --------------------------------
+
 // --------- TASKS CONFIG ------------
 #define OBSTACLE_TASK_PERIOD 150
+#define FOLLOW_LINE_TASK_PERIOD 100
+
+enum task_priorities {
+  LED_PRIO = 0,
+  COMMS_PRIO,
+  PING_PRIO,
+  FOLLOW_LINE_PRIO,
+  OBSTACLE_PRIO
+};
 // -----------------------------------
 
+// --------- FOLLOW LINE UTILS ---------
+#define V_REF 100
+#define V_MIN 20
+
+// Line states [true = black (line), false = white (no line)]       !!!!! SERGIO LEE ESTO !!!!!
+#define LEFT          "100"
+#define CENTER_LEFT   "110"
+#define CENTER_ALL    "111"
+#define CENTER_ONLY   "010"
+#define CENTER_RIGHT  "011"
+#define RIGHT         "001"
+#define NO_LINE       "000"
+
+#define K_HIGH 50.0 // TODO (para el blog) Para 001 ó 100
+#define K_LOW K_HIGH/2 // TODO (para el blog) Para 011 ó 110
+#define K_NULL 0.0 // TODO (para el blog) Para 111 ó 010
+// -------------------------------------
+
+// -------- SERIAL CONFIG ----------
+
+// ---------------------------------
 #endif CONFIG_H
--- src/follow_line_arduino/follow_line_arduino.ino
+++ src/follow_line_arduino/follow_line_arduino.ino
@@ -1,26 +1,76 @@
+#include <string.h>
+
 #include <Arduino_FreeRTOS.h>
 #include "FastLED.h"
 
 #include "config.h"
 
-const int rgb_red[] = RED;
-const int rgb_green[] = GREEN;
+// +++++ CONSTANTS +++++
+#define SEC_TO_MS 1000.0
 
-void obstacleTask(void *params);
-void followLineTask(void *params);
+// +++++ FUNCTONS DEFINITION +++++
+void obstacle_task(void *params);
+void follow_line_task(void *params);
+
+// +++++ VARIABLES +++++
+const int RGB_RED[RGB_COMPONENTS] = RED;
+const int RGB_GREEN[RGB_COMPONENTS] = GREEN;
+
+const int IR_SENSORS[N_IR_SENSORS] = IR_SENSORS_LIST;
 
 CRGB leds[NUM_LEDS];
 
+TaskHandle_t follow_line_task_handle; // TODO mencionarlo en el BLOG
+
+// +++++ MAIN PROGRAM +++++
 void setup() {
   Serial.begin(9600);
   
   while(!Serial); // Wait until Serial port is intialized
 
-  FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
-  FastLED.setBrightness(20);
+  // Motors
+  pinMode(PIN_Motor_STBY, OUTPUT);
+  
+  pinMode(PIN_Motor_AIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMA, OUTPUT);
 
+  pinMode(PIN_Motor_BIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMB, OUTPUT);
+
+  analogWrite(PIN_Motor_PWMA, MOTOR_STOP);
+  analogWrite(PIN_Motor_PWMB, MOTOR_STOP);
+
+  digitalWrite(PIN_Motor_STBY, HIGH); // TODO (a lo mejor) deshabilitarlos hasta que se sincronice la ESP32
+
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+
+  // Neopixel led
+  FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
+  FastLED.setBrightness(0);
+
+  // Ultrasonic sensor
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+
+  // Tasks initialization
+  xTaskCreate(
+    follow_line_task,
+    "follow_line_task",
+    100,
+    NULL,
+    FOLLOW_LINE_PRIO,
+    &follow_line_task_handle
+  );
+
+  xTaskCreate(
+    obstacle_task,
+    "obstacle_task",
+    100,
+    NULL,
+    OBSTACLE_PRIO,
+    NULL
+  );
 }
 
 void loop() {
@@ -28,26 +78,113 @@ void loop() {
 
 }
 
-void followLineTask(void *params) {
+// +++++ FUNCITONS DECLARATION +++++
+
+void follow_line_task(void *params) {
   (void) params;
 
+  TickType_t x_last_wake_time;
+  float kr = K_NULL, kl = K_NULL, vr, vl;
+  unsigned long time_lost_start = 0, time_lost = 0;
 
-}
-
-void obstacleRead(void *params) {
-  (void) params;
-
-  TickType_t xLastWakeTime;
-  float dist;
-  unsigned long last, now;
+  String line_state = NO_LINE;
 
   while (true) {
-    xLastWakeTime = xTaskGetTickCount();
+    x_last_wake_time = xTaskGetTickCount();
 
-    now = millis();
-    Serial.print("us: ");
-    Serial.println(now - last);
-    last = now;
+    Serial.println("---------");
+
+    for (int i = 0; i < N_IR_SENSORS; i++) {
+      //Serial.println(analogRead(IR_SENSORS[i]));
+
+      line_state[i] =
+        analogRead(IR_SENSORS[i]) >= IR_BLACK_THRESHOLD ? '1' : '0';  // !!!!! SERGIO !!!!! 1 = la línea está ahí (la linea está justo debajo del igual por el estilo)
+    }
+
+    Serial.println(line_state);
+
+    if (line_state == LEFT) {
+      kr = K_HIGH;
+      kl = K_NULL;
+
+      if (time_lost == 0) {
+        time_lost_start = millis();
+      }
+
+      time_lost = millis() - time_lost_start;
+
+    } else if (line_state == RIGHT) {
+      kr = K_NULL;
+      kl = K_HIGH;
+
+      if (time_lost == 0) {
+        time_lost_start = millis();
+      }
+
+      time_lost = millis() - time_lost_start;
+
+    } else if (line_state == CENTER_LEFT) {
+      kr = K_LOW;
+      kl = K_NULL;
+
+      if (time_lost == 0) {
+        time_lost_start = millis();
+      }
+
+      time_lost = millis() - time_lost_start;
+
+    } else if (line_state == CENTER_RIGHT) {
+      kr = K_NULL;
+      kl = K_LOW;
+
+      if (time_lost == 0) {
+        time_lost = millis();
+      }
+
+      time_lost = millis() - time_lost_start;
+
+    } else if (line_state == CENTER_ONLY || line_state == CENTER_ALL) {
+      kr = K_NULL;
+      kl = K_NULL;
+
+      time_lost = 0;
+
+    } else {
+      time_lost = millis() - time_lost_start;
+    }
+
+    // !!!!! SERGIO !!!!! Quizás hay que poner algo de que la velocidad no vuelva a ser directamente V_REF segun vuelve a detectar que va por el medio
+
+    vr = V_REF - (kr * (time_lost / SEC_TO_MS)); // !!!!! SERGIO !!!!! Para que no sea tan basto lo hacemos con los segundos y no con los milisegundos
+    vl = V_REF - (kl * (time_lost / SEC_TO_MS));
+
+    if (vr < V_MIN) {
+      vr = V_MIN;
+    }
+
+    if (vl < V_MIN) {
+      vl = V_MIN;
+    }
+    
+    //analogWrite(PIN_Motor_PWMA, vr); // NO PROBADO BIEN (pero parece estar bien por las trazas)
+    //analogWrite(PIN_Motor_PWMB, vl);
+
+    Serial.println(vr);
+    Serial.println(vl);
+
+    xTaskDelayUntil(&x_last_wake_time, FOLLOW_LINE_TASK_PERIOD / portTICK_PERIOD_MS);
+  }
+}
+
+void obstacle_task(void *params) {
+  (void) params;
+
+  TickType_t x_last_wake_time;
+  float dist;
+  bool is_finished = false;
+
+  while (!is_finished) {
+    x_last_wake_time = xTaskGetTickCount();
 
     digitalWrite(TRIG_PIN, HIGH);
     delayMicroseconds(TRIG_DELAY);
@@ -55,8 +192,15 @@ void obstacleRead(void *params) {
     
     dist = (pulseIn(ECHO_PIN, HIGH) / 2) * SOUND_SPEED; // Unit: cm
 
-    Serial.print("dist: ");
-    Serial.println(dist);
-    xTaskDelayUntil(&xLastWakeTime, PERIOD_US_TASK / portTICK_PERIOD_MS);
+    if (dist <= OBSTACLE_DIST_LIMIT) {
+      //vTaskSuspend(follow_line_task_handle);               // !!!!! SERGIO !!!!! Lo he comentado ahora para probar con trazas lo de los IR
+
+      analogWrite(PIN_Motor_PWMA, MOTOR_STOP);
+      analogWrite(PIN_Motor_PWMB, MOTOR_STOP);
+
+      is_finished = true; // TODO (quitar comentario) EN CASO DE QUE EL PROGRAMA TENGA QUE ACABAR CUANDO SE LLEGUE AL OBSTÁCULO
+    }
+   
+    xTaskDelayUntil(&x_last_wake_time, OBSTACLE_TASK_PERIOD / portTICK_PERIOD_MS);
   }
 }
