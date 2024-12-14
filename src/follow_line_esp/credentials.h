9f04106f323df064c970b51e1f39c875e1ed09f7
scobosb <s.cobosb.2022@alumnos.urjc.es>
2024-12-14 19:27:50 +0100

    Updated credentials

--- src/follow_line_esp/credentials.h
+++ src/follow_line_esp/credentials.h
@@ -1,7 +1,10 @@
-#define WLAN_SSID "ssid"
-#define WLAN_PASS "pwd"
+#define WLAN_SSID "SSID"
+#define WLAN_PASS "PWD"
 
 #define USERNAME "username"
 #define KEY      "key"
 
-
+#define EAP_ANONYMOUS_IDENTITY "anonymous@urjc.es" // leave as it is
+#define EAP_IDENTITY "USER@urjc.es"    // Use your URJC email
+#define EAP_PASSWORD "PASSWORD"            // User your URJC password
+#define EAP_USERNAME "USER@urjc.es"    // Use your URJC email
